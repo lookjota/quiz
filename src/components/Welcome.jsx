842893b958a47abccba14bbdc201fb53cdd08ae2
lookjota <lookjota@gmail.com>
2023-02-16 10:46:38 -0300

    context creation

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useContext } from 'react'
+import { QuizContext } from '../context/quiz'
 
 import Quiz from '../img/quiz.svg'
 
 import "./Welcome.css"
 
 
+
 function Welcome() {
+
+  const quizState = useContext(QuizContext);
+
+  console.log(quizState)
+
   return (
     <div id="welcome">
         <h2>Seja bem-vindo</h2>
